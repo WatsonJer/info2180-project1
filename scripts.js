370bf67ff4e2ab455a38f921e0e80967dd3410df
Jeremy Watson <watsonjeremy92@gmail.com>
2025-11-02 23:36:06 -0500

    Finished javascript and slight change to padding

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 /* Add your JavaScript to this file */
-document.addEventListener("DOMContentLoaded", function () {});
+window.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".newsletter form");
+  const emailInput = document.getElementById("email");
+  const message = document.querySelector(".message");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = emailInput.value.trim();
+
+    if (email === "") {
+      message.textContent = "Please enter a valid email address.";
+    } else {
+      message.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+      emailInput.value = "";
+    }
+  });
+});
